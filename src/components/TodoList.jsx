710c5632cd6a2c6a26890b61a875cbe9a57165bc
Yvener <146208496+ybazile@users.noreply.github.com>
2024-07-11 20:07:52 -0400

    Update TodoList.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
-import TodoCard from './TodoCard'
+import React, { useState } from 'react';
+import TodoCard from './TodoCard';
 
 export default function TodoList(props) {
-    const { todos } = props
+    const { todos, handleEditTodo, handleDeleteTodo } = props;
+    const [completedTodos, setCompletedTodos] = useState([]);
+
+    const toggleComplete = (index) => {
+        const updatedCompletedTodos = [...completedTodos];
+        if (updatedCompletedTodos.includes(index)) {
+            updatedCompletedTodos.splice(updatedCompletedTodos.indexOf(index), 1);
+        } else {
+            updatedCompletedTodos.push(index);
+        }
+        setCompletedTodos(updatedCompletedTodos);
+    };
+
     return (
-        <ul className ='main'>
-            {todos.map((todo, todoIndex)=>{
-                return (
-                    <TodoCard {...props} key={todoIndex} index = {todoIndex}>
-                        <p>{todo}</p>
-                    </TodoCard>
-                    
-                )
-            })}
+        <ul className='main'>
+            {todos.map((todo, todoIndex) => (
+                <TodoCard
+                    {...props}
+                    key={todoIndex}
+                    index={todoIndex}
+                    isComplete={completedTodos.includes(todoIndex)}
+                    toggleComplete={toggleComplete}
+                >
+                    <p>{todo}</p>
+                </TodoCard>
+            ))}
         </ul>
-    )
+    );
 }
