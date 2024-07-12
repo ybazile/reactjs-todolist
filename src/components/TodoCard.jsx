593fe381e4b1558c745cde28ef403336b4ce8c7d
Yvener <146208496+ybazile@users.noreply.github.com>
2024-07-11 21:27:50 -0400

    Update TodoCard.jsx

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
+import React from "react";
 
 export default function TodoCard(props) {
-    const { children, handleDeleteTodo, index, handleEditTodo, isComplete, toggleComplete } = props;
+  const { children, handleDeleteTodo, index, handleCompleteTodo, handleIncompleteTodo, handleEditTodo, isComplete, isIncomplete } = props;
 
-    return (
-        <li className={`todoItem ${isComplete ? 'completed' : ''}`}>
-            <div className="todoContent" onClick={() => toggleComplete(index)}>
-                {children}
-            </div>
-            <div className="actionsContainer">
-                <button onClick={() => handleEditTodo(index)}>
-                    <i className="fa-solid fa-pen-to-square"></i>
-                </button>
-                <button onClick={() => handleDeleteTodo(index)}>
-                    <i className="fa-solid fa-trash-can"></i>
-                </button>
-            </div>
-        </li>
-    );
+  return (
+    <li
+      className={`todoItem ${isComplete ? 'complete' : ''} ${isIncomplete ? 'incomplete' : ''}`}
+      onClick={() => handleCompleteTodo(index)}
+      onDoubleClick={() => handleIncompleteTodo(index)}
+    >
+      {children}
+      <div className="actionsContainer">
+        <button
+          onClick={(e) => {
+            e.stopPropagation(); // Prevents triggering the li's onClick
+            handleEditTodo(index);
+          }}
+        >
+          <i className="fa-solid fa-pen-to-square"></i>
+        </button>
+        <button
+          onClick={(e) => {
+            e.stopPropagation(); // Prevents triggering the li's onClick
+            handleDeleteTodo(index);
+          }}
+        >
+          <i className="fa-solid fa-trash-can"></i>
+        </button>
+      </div>
+    </li>
+  );
 }
